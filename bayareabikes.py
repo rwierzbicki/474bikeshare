fe6007e9c8465f37cb5c6256de10ef0cece87242
mbh <ramwierzbicki@gmail.com>
2016-11-14 19:07:31 -0800

    cleaned bayareabikes.py

--- bayareabikes.py
+++ bayareabikes.py
@@ -10,36 +10,24 @@ import time
 ...         print(', '.join(row))
 """
 ##
-tripcount = [[0 for i in range(32)] for j in range(13)]
+months = ['no', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+
+tripcount = [[0 for i in range(0, 32, 1)] for j in range(0, 13, 1)]
 
 with open("201608_trip_data.csv") as csvfile:
     testreader = csv.reader(csvfile)
-    test = "9/2/2015"
-    ##print(test.split('/')[1])
-    #tripcount = [][]
-    sample = ""
-    count = 0
-    ##while day
+
+
     for row in testreader:
         temp = next(testreader)[2][:9].split('/')
         day = int(temp[1])
         month = int(temp[0])
-        print(day)
-
+        #print(day,month)
         tripcount[month][day] =1+tripcount[month][day]
 
 
-    for i in range(13):
+    for i in range(1,13):
+        print(months[i])
         print(tripcount[i])
 
-    while sample!=test:
-        sample = next(testreader)[2][:8]
-        #print("yea")
-        count = count+1
-
-    print(count)
-    for x in range(1, 5):
-        ##print(x)
-        ##print(testreader.__next__())
-        print(int(next(testreader)[2][:9].split('/')[1]))
-        ##testreader.__next__()
