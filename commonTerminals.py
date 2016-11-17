3b694b4f0b4909911b5b55996e8b3456e88a3b31
mbh <ramwierzbicki@gmail.com>
2016-11-17 00:19:41 -0800

    made it more pythonic, because we're supposed to be better nerds than this...

--- commonTerminals.py
+++ commonTerminals.py
@@ -1,23 +1,34 @@
+
+import sys
+
 import csv
 
+import matplotlib.pyplot as plt
+import numpy as np
 
 
+print(sys.version)
+filename = "201608_trip_data.csv"
 
 
+def tripcounter(datafile):
+    tripcount = [[0 for i in range(0, 100, 1)] for j in range(0, 100, 1)]
+    with open(datafile) as csvfile:
+        testreader = csv.reader(csvfile)
 
-tripcount = [[0 for i in range(0, 100, 1)] for j in range(0, 100, 1)]
-
-with open("201608_trip_data.csv") as csvfile:
-    testreader = csv.reader(csvfile)
-
-    print(next(testreader))
-    for row in testreader:
-        start_terminal = int(row[4])
-        end_terminal = int(row[7])
-        tripcount[start_terminal][end_terminal] =1+tripcount[start_terminal][end_terminal]
+        print(next(testreader))
+        for row in testreader:
+            start_terminal = int(row[4])
+            end_terminal = int(row[7])
+            tripcount[start_terminal][end_terminal] =1+tripcount[start_terminal][end_terminal]
 
 
-    for i in range(1,100):
-        print(i)
-        print(tripcount[i])
+        for i in range(1,100):
+            print(i)
+            print(tripcount[i])
 
+    plt.imshow(tripcount, cmap=plt.cm.gray)
+    plt.colorbar()
+    plt.show()
+
+tripcounter(filename)
