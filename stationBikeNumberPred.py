7c3174063a5b923395600662a7d9ab445546ffae
colemcginn <colemcginn@hotmail.com>
2016-11-20 18:34:01 -0800

    predictor complete using multinum naive bayes

--- stationBikeNumberPred.py
+++ stationBikeNumberPred.py
@@ -10,41 +10,49 @@ emptytime = [[0 for i in range(0, 13, 1)] for j in range(0, 100, 1)]
 start_time = time.time()
 filename = "201608_status_data.csv"
 
+row_count = 589079 # int(35517186)
+x_train = [[0,0,0] for j in range(row_count)] # day of week, hour, station #
+yBike_train = [0 for j in range(row_count)] # bikes avail
+yDock_train = [0 for j in range(row_count)] # docks avail
 def timeWithNumberOfBikes(datafile, numBikes=3):
+
     with open(datafile) as csvfile:
         testreader = csv.reader(csvfile)
-        row_count = sum(1 for row in testreader)
-        x_train = [[0 for i in range(3)] for j in range(row_count)] # day of week, hour, station #
-        yBike_train = [0 for j in range(row_count)] # bikes avail
-        yDock_train = [0 for j in range(row_count)] # docks avail
+        print ("Starting")
         print(next(testreader))
-        print("loop now")
+        # row_count = sum(1 for row in testreader)
+        # print ("Counted", row_count)
+        
         count = 0
+        print ("loop")
         for row in testreader:
-            # min = int(row[3].split('/')[2].split(' ')[1].split(':')[1])
-            # if(min==0):
-            count+=1
-            stationNumber = int(row[0])
-            month = int(row[3][:9].split('/')[0])
-            day = int(row[3][:9].split('/')[1])
-            year = int(row[3].split('/')[2].split(' ')[0])
-            hour = int(row[3].split('/')[2].split(' ')[1].split(':')[0])
-            min = int(row[3].split('/')[2].split(' ')[1].split(':')[1])
-            # print month, day, year, hour, min
+            # print ("lol")
+            # print (row[0])
+            mins = int(row[3].split('/')[2].split(' ')[1].split(':')[1])
+            if(mins==0):
+                count+=1
+                # print(count)
+                stationNumber = int(row[0])
+                month = int(row[3][:9].split('/')[0])
+                day = int(row[3][:9].split('/')[1])
+                year = int(row[3].split('/')[2].split(' ')[0])
+                hour = int(row[3].split('/')[2].split(' ')[1].split(':')[0])
+                mins = int(row[3].split('/')[2].split(' ')[1].split(':')[1])
+                # print (month, day, year, hour, mins)
 
-            bikesAvail = int(row[1])
-            docksAvail = int(row[2])
-            x_train[0][count] =  weekDay(year,month,day)
-            x_train[1][count] = hour
-            x_train[2][count] = stationNumber
+                bikesAvail = int(row[1])
+                docksAvail = int(row[2])
+                x_train[count][0] = weekDay(year,month,day)
+                x_train[count][1] = hour
+                x_train[count][2] = stationNumber
 
-            yBike_train[count] = bikesAvail
-            yDock_train[count] = docksAvail
+                yBike_train[count] = bikesAvail
+                yDock_train[count] = docksAvail
 
-            # if bikesAvail<=numBikes:
-            #     emptytime[stop][month] +=1
-            # if count%100000==0:
-            #         print(count)
+                # if bikesAvail<=numBikes:
+                #     emptytime[stop][month] +=1
+                if count%100000==0:
+                        print(count)
                 # print(isinstance(int(row[1]), int ))
 
     # stopid =0
@@ -80,15 +88,36 @@ def weekDay(year, month, day):
     dayOfWeek += offset[month - 1] + (day - 1)
     dayOfWeek %= 7
     return dayOfWeek #, week[dayOfWeek]
-
+# x_test = [[1,9,4],[4,6,4]]
+# print (x_test[0])
 timeWithNumberOfBikes(filename)
+
 clf = MultinomialNB()
 clf.fit(x_train, yBike_train)
-x_test = [[1,9,4],[4,6,4]]
+x_test = [[1,9,4],
+    [4,18,4],
+    [5,17,13],
+    [3,1,1],
+    [6,20,52],
+    [7,12,62],
+    [1,16,91],
+    [2,10,16]
+]
+
+# print (x_train[0:2][0])
+# print (x_test[0])
 MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True)
 print(clf.predict(x_test))
 clf2 = MultinomialNB()
-clf.fit(x_train, yDock_train)
-x_test = [[1,9,4],[4,6,4]]
+clf2.fit(x_train, yDock_train)
+x2_test = [[1,9,4],
+    [4,18,4],
+    [5,17,13],
+    [3,1,1],
+    [6,20,52],
+    [7,12,62],
+    [1,16,91],
+    [2,10,16]
+]
 MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True)
-print(clf.predict(x_test))
+print(clf2.predict(x2_test))
